301b379c17e873bb6f9bdb417c5fe89e21085818
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-03-19 08:50:34 -0700

    Add Test calculator code

--- src/test/groovy/com/victoralfonsoulloa/calculator/CalculatorTest.java
+++ src/test/groovy/com/victoralfonsoulloa/calculator/CalculatorTest.java
@@ -1,25 +1,43 @@
 package com.victoralfonsoulloa.calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class CalculatorTest {
 
+    Calculator calculator;
     @BeforeEach
     void setUp() {
+        calculator = new Calculator();
     }
 
     @Test
-    void add() {
+    @DisplayName("Test for simple Addition")
+    void testAdd() {
+        assertEquals(3, calculator.add(1,2), "Simple addition");
+        assertEquals(-2, calculator.add(-4,2), "Adds a negative number");
     }
 
     @Test
-    void subtract() {
+    @DisplayName("Test for simple Subtraction")
+    void testSubtract() {
+        assertEquals(2, calculator.subtract(4,2), "Four minus two");
+        assertEquals(4, calculator.subtract(5,1), "Five minus one");
     }
 
     @Test
-    void divide() {
+    @DisplayName("Test for simple Division")
+    void testDivide() {
+        assertEquals(2, calculator.divide(6,3), "6/3");
+        assertEquals(5, calculator.divide(5,1), "5/1");
     }
+    @Test
+    @DisplayName("Division by zero should throw an ArithmeticException")
+    void testDivideByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+        assertEquals("Cannot divide by zero", exception.getMessage());
+    }
+
 }
